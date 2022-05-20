75eddec307e2565e25825e9d12261c58f1066445
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-05-20 11:45:42 +0200

    destroy solved

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,9 +18,8 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
-    @list = @bookmark.list
-    @bookmark.delete
-    redirect_to list_path(@list), status: :see_other
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
